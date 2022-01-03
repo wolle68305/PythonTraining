95325a1eda1ff903609bcbd871accf3b175f52d3
wolle68305 <wolf_daniel24@hotmail.com>
2022-01-03 15:50:00 +0100

    Listen erweitert

--- Listen.py
+++ Listen.py
@@ -1,15 +1,24 @@
 def start_main():
     liste1 = []
-    liste1 = fill_List(liste1)
+    liste2 = [] 
+    #liste1 = fill_List(liste1)
+    liste1  = fill_List2(liste1)
+    #Ausgaben der Ergebnisse der Funktionen
     print_List(liste1)
-    print(return_LastListElement(liste1))
-    print(return_FirstListElement(liste1))
-    print(list_slicing1(liste1))
-    print(list_slicing2(liste1))
-    print(list_slicing3(liste1))
+    #print(return_LastListElement(liste1))
+    #print(return_FirstListElement(liste1))
+    #print(list_slicing1(liste1))
+    #print(list_slicing2(liste1))
+    #print(list_slicing3(liste1))
+    print(list_comprehensions(liste1,liste2))
+    print(list_comprehensions2(liste1,liste2))
 
 def fill_List(liste1):
     liste1 = ["gut", "hilfreich", "besser", "optimal"]
+    return liste1
+
+def fill_List2(liste1):
+    liste1 = [1, 2, 3, 4, 5, 6, 7, 8]
     return liste1
 
 def set_List2(liste1):
@@ -29,6 +38,7 @@ def return_FirstListElement(liste):
 
 def list_slicing1(liste):
     #Ausgabe "hilfreich, besser"
+    #Doppelpunkt bedeutet List Slicing
     return liste[1:3]
 
 def list_slicing2(liste):
@@ -39,6 +49,16 @@ def list_slicing2(liste):
     
 def list_slicing3(liste):
     #Ausgabe "hilfreich, besser, optimal"
-    
+    #bis auf den ersten Wert wird alles ausgegeben
     return liste[1:]    
+
+def list_comprehensions(liste1 , liste2):
+    for x in liste1:
+        liste2.append(x*x)
+    return liste2    
+
+def list_comprehensions2(liste1, liste2):
+    return [x * x for x in liste1]
+
+
 start_main()
