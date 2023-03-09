376d5dcc4c31f1da9e42e928aafda9d92ad281aa
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2023-03-09 18:36:15 +0100

    Continued with tupels

--- Tupel.py
+++ Tupel.py
@@ -7,11 +7,22 @@ def main():
     print(vTupel)
     print(vTupel,0)
 
+    name, age, job = returnMultipleValuesFromTupel()
+    #gibt mehrere Werte zurück und schreibt diese in die entsprechenden Variablen
+
 def create_Tupel(affectedTupel):
-    affectedTupel = (1,2,3)
+    affectedTupel = ("Daniel Wolf",34,"Informatiker")
     return affectedTupel
 
 def return_SingleValueFormTupel(affectedTupel, positionOfReturnValue):
     return affectedTupel[positionOfReturnValue]
+
+def easyWayValuesFromTupelToVariables(affectedTupel):
+    name, age, job = affectedTupel
+    #schreibt innerhalb einer Zeile die Werte des Tuppels in die 3 Variablen
+    #Anzahl der Variablen muss genauso viel sein wie die Anzahl der Elemente in einem Tupel
+
+def returnMultipleValuesFromTupel():
+    return ("Max Mustermann", 40, "Informatiker")
 
 main()
