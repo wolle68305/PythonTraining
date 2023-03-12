de269f1fb921c5b48dbfe30b78e3f809fd443d1f
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2023-03-12 12:02:37 +0100

    add function4

--- Tupel.py
+++ Tupel.py
@@ -7,8 +7,10 @@ def main():
     print(vTupel)
     print(vTupel,0)
 
-    name, age, job = returnMultipleValuesFromTupel()
     #gibt mehrere Werte zurück und schreibt diese in die entsprechenden Variablen
+    name, age, job = returnMultipleValuesFromTupel()
+
+    function4()
 
 def create_Tupel(affectedTupel):
     affectedTupel = ("Daniel Wolf",34,"Informatiker")
@@ -25,4 +27,20 @@ def easyWayValuesFromTupelToVariables(affectedTupel):
 def returnMultipleValuesFromTupel():
     return ("Max Mustermann", 40, "Informatiker")
 
+def function4():
+    students = [
+        ("Max Müller", 22),
+        ("Monika Mustermann", 23)
+    ]
+
+    #Variante 1
+    #for student in students:
+        #name, age = student
+        #print(name)
+        #print(age)
+
+    #variante 2
+    for name, age in students:
+        print(name)
+        print(age)    
 main()
