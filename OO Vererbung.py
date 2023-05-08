06ef4b3f5db49164474b5a5d1bac50010de4878b
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2023-05-08 19:34:24 +0200

    Continue mit Vererbung

--- OO Vererbung.py
+++ OO Vererbung.py
@@ -1,4 +1,5 @@
 class Student():
+    #Allgemeine Student Klasse, die von der WorkingStudent Klasse vererbt wird
     def __init__(self, firstName, surName):
         self.firstName = firstName
         self.surName = surName
@@ -8,13 +9,29 @@ class Student():
     
 class WorkingStudent(Student):
     #durch das Student in der Klasse wird gesagt, dass alles von der Klasse Student geerbt werden soll
+    #bei der Print Ausgabe hat man daher Zugriff auf die Methode name für den Student2 (WorkingStudent)
     def __init__(self, firstName, surName, company):
-        self.firstName = firstName
-        self.surName = surName
+        #self.firstName = firstName
+        #self.surName = surName
+        #um firstName und surName nicht zweimal schreiben zu müssen, kann man auch die init der Student Klasse aufrufen,
+        #von der die Klasse WorkingStudent erbt (von der Elternklasse)
+        super().__init__(firstName, surName)
         self.company = company
-     
+
+    def name(self):
+        #Überschreibt die Methode name der Klasse Student
+        return "WorkingStudent:" + self.firstName + " " + self.surName 
     
+    def name2(self):
+        #Alternative, greift auf die Methode name der Student Klasse zu und gibt Vorname und Nachname zurück
+        return super().name() + " (" + self.company + ")"
+     
+#Standard Student Objekt wird erstellt    
 student = Student("Max", "Müller")
+
+#Working Student Objekt wird erstellt
 student2 = WorkingStudent("Daniel", "Wolf", "Wolf GmbH")
+
 print(student.name())    
 print(student2.name())
+print(student2.name2())
