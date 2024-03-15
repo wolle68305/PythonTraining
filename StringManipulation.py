f959d9a9327b7ef3af06589076cc2a331185a504
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2024-03-15 17:04:50 +0100

    String Methoden hinzugefügt

--- StringManipulation.py
+++ StringManipulation.py
@@ -1,9 +1,11 @@
 def main():
-    vS_Text  = "Hallo Welt"
-    print(substring_Function(vS_Text))
-    print(substring_Function2(vS_Text))
-    print(substring_Function3(vS_Text))
-    print(partOfTheString(vS_Text))
+    #vS_Text  = "Hallo Welt"
+    #print(substring_Function(vS_Text))
+    #print(substring_Function2(vS_Text))
+    #print(substring_Function3(vS_Text))
+    #print(partOfTheString(vS_Text))
+    #lineBreak()
+    fStringMethode()
 
 def substring_Function(vS_Text):
     #Ausgabe "Hallo"
@@ -22,6 +24,17 @@ def substring_Function3(vS_Text):
 def partOfTheString(vS_Text):
     #Ausgabe a
     return vS_Text[1]
+
+def lineBreak():
+    print("Hallo \nWelt")
+
+def fStringMethode():
+    name = "Daniel Wolf"
+    alter = 36
+    #f String wandelt alle Variablen in ein String um
+    #mit dem Backslash kann man bei der Codezeile einen Zeilenumbruch verwenden
+    print(f"Hier kommt die Einladung für {name} "+ \
+          f"zu seinem {alter}. Geburtstag")
 
 main()
 
