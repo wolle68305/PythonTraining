49988f27c351f314401ec2ae67e2dc19796640c8
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2024-03-17 22:19:08 +0100

    Working With File is now working with a file on a mounted network drive

--- WorkingWithFile.py
+++ WorkingWithFile.py
@@ -1,10 +1,19 @@
+import os
 def main():
-    with open('gaeste.txt', 'r', encoding='utf-8') as file:
+    pathToFile = "/home/daniel/TransferSynology/Python Training/gaeste.txt"
+    method1(pathToFile)
+    #method2(pathToFile)
+    #getCurrentFolderLocation()
+
+def method1(pathToFile):
+    f = open(pathToFile , "r")
+    print(f.read())
+
+def method2(pathToFile):
+    with open(pathToFile, 'r', encoding='utf-8') as file:
         print(file)
 
-
-
-
-
+def getCurrentFolderLocation():
+    print(os.getcwd())
 
 main()
