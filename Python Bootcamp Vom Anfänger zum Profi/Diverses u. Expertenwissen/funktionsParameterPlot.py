062dc16232a0740c121e1cb7734f70052590bf1d
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2024-07-29 17:06:04 +0200

    Add comment

--- Python Bootcamp Vom Anfänger zum Profi/Diverses u. Expertenwissen/funktionsParameterPlot.py
+++ Python Bootcamp Vom Anfänger zum Profi/Diverses u. Expertenwissen/funktionsParameterPlot.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 
 def create_Plot(**plot_params):
+    #plot_params Parameter ist ein Dictionary mit den entsprechenden Werten/Parametern für die Plot Funktion
     print(plot_params)
     plt.plot([1,2,3], [5,6,5], **plot_params)
     plt.show()
