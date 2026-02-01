03d8a7feb58520d2ead3cc66e1514a72bb61c376
wolle68305 <57529275+wolle68305@users.noreply.github.com>
2026-02-01 11:48:46 +0100

    Add files via upload

--- mariaDBTesting.py
+++ mariaDBTesting.py
@@ -1,17 +1,19 @@
-import mariadb
+import mysql.connector as mariadb
 
-con = mariadb.connect(
-    user='testUser',
-    host= '192.168.188.26',
-    database = 'testdb',
-    passwd = 'Passwd123!',
-    port = 3307
-)
+try:
+    conn = mariadb.connect(
+        user = "testUser",
+        password = "Passwd123!",
+        host = "192.168.188.26",
+        database = "testdb",
+        port = 3307
+    )
 
-mycursor = con.cursor()
+    cur = conn.cursor()
+    cur.execute("insert into testTable1 (Column1) Values (""Test Python"")")
+    #cur.execute("select * from testTable1")
 
-mycursor.execute("SHOW DATABASES;")
-for i in mycursor:
-    print(i)
-
-con.commit()
+    #for (Column1, Column2) in cur:
+    #    print(f" {Column1} {Column2}")
+except mariadb.Error as e:
+    print(f"Error: {e}")
